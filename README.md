0550a586a2ee796150b7f93371f792f2bd7b8377
jos3ric4rd0 <lol.ji@hotmail.com>
2026-04-12 03:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ README.md
@@ -0,0 +1,2 @@
+### Projeto Técnico: Sistema de Notas Fiscais
+Status: Em desenvolvimento (Microsserviço de Estoque concluído).
---
+++ SistemaEstoque.API/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaEstoque.API.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        public int Saldo { get; set; }
+    }
+}
